08781050de865841f2b3c7cb86d78c85ed9a7228
nnotMarss <m4rs_fox@outlook.com.au>
2024-03-04 02:44:24 +1100

    24v09Cc

--- $WEBSERVER/xyz.notmars.web/_BTV_/saturnNotif.js
+++ $WEBSERVER/xyz.notmars.web/_BTV_/saturnNotif.js
@@ -2,7 +2,7 @@ function showNotification(id) {
     const notification = document.getElementById(id);
     notification.classList.add('show');
   }
-  
+
   function hideNotification(id) {
     const notification = document.getElementById(id);
     notification.classList.remove('show');
